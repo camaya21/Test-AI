f7ee3194eb5177c67e16c97ce492c8dcebadf1d7
camaya21 <cindymamaya21@gmail.com>
2023-08-15 20:19:29 -0400

    created userInterface

--- script.js
+++ script.js
@@ -2,15 +2,23 @@ import {config} from "dotenv"
 config()
 
 import {Configuration, OpenAIApi} from "openai"
+import readline from "readline"
 
 const {API_KEY} = process.env
 
 const openai = new OpenAIApi(new Configuration({
-    apiKey:  process.env
+    apiKey:  API_KEY
 }))
+
+const userInterface = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
 
 openai.createChatCompletion({
     model: "gpt-3.5-turbo",
-    messages: [{ role: "user", content: "hello ChatGPT"}]
-
+    messages: [{ role: "user", content: "Hello ChatGPT"}],
+})
+.then(res => {
+    console.log(res.data.choices[0].message.content)
 })
